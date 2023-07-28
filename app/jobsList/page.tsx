4979ca22332550b0353818546c948709144b1537
shellyalmo <shellyalmo@gmail.com>
2023-07-28 16:46:56 +0300

    initial commit

---
+++ app/jobsList/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1>List of all jobs</h1>
+      </main>
+    )
+  }
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { SignIn } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Welcome to jobs boards for juniors</h1>
+      <SignIn redirectUrl="jobsList"/>
+      <UserButton afterSignOutUrl="/"/>
+    </main>
+  )
+}
