bf2e7b5d2a4c3225a0e5b7bf61aeaa1a70684aec
shellyalmo <shellyalmo@gmail.com>
2023-08-04 18:28:02 +0300

    add recruiter form

--- app/register/recruiter/page.tsx
+++ app/register/recruiter/page.tsx
@@ -1,5 +1,42 @@
+'use client'
+import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import SendIcon from '@mui/icons-material/Send';
+
+const skills = [
+    'React', 'Angular', 'Vue', 'CSS', 'HTML', 'Javascript', 'Typescript', 'MongoDB', 'Node.js']
+
 export default function Recruiter() {
+
     return (
-        <>hi recruiter</>
+        <div>
+            <h1>Enter job details:</h1>
+            <form action="" autoComplete="off" className="flex flex-col space-y-4">
+                <TextField id="job-title" label="Job Title" size="small" variant="outlined" />
+                <TextField id="job-location" label="Job Location" size="small" variant="outlined" />
+                <TextField id="job-description" label="Job Description" variant="outlined" />
+                <Stack spacing={3} sx={{ width: 500 }}>
+                    <Autocomplete
+                        multiple
+                        id="tags-standard"
+                        options={skills}
+                        getOptionLabel={(option) => option}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                label="Job Skills"
+                                placeholder="Skills"
+                            />
+                        )}
+                    />
+                </Stack>
+                <Button size="small" type="submit" variant="contained" endIcon={<SendIcon />}>
+                    Submit
+                </Button>
+            </form>
+        </div>
     )
 }
