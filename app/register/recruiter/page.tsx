d0af88fd339805392804a4a8f687278e11e928fe
shellyalmo <shellyalmo@gmail.com>
2023-08-05 18:49:35 +0300

    feat: recruiter job form json

--- app/register/recruiter/page.tsx
+++ app/register/recruiter/page.tsx
@@ -1,42 +1,88 @@
 'use client'
-import Autocomplete from "@mui/material/Autocomplete";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import SendIcon from '@mui/icons-material/Send';
+// import Autocomplete from "@mui/material/Autocomplete";
+// import Button from "@mui/material/Button";
+// import Stack from "@mui/material/Stack";
+// import TextField from "@mui/material/TextField";
+// import SendIcon from '@mui/icons-material/Send';
 
-const skills = [
-    'React', 'Angular', 'Vue', 'CSS', 'HTML', 'Javascript', 'Typescript', 'MongoDB', 'Node.js']
+import Form from '@rjsf/mui';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv6';
 
+const schema: RJSFSchema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "additionalProperties": false,
+    "definitions": {
+        "iso8601": {
+            "type": "string",
+            "description": "Similar to the standard date type, but each section after the year is optional. e.g. 2014-06-29 or 2023-04",
+            "pattern": "^([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]|[1-2][0-9]{3}-[0-1][0-9]|[1-2][0-9]{3})$"
+        }
+    },
+    "properties": {
+        "Job Info": {
+            "type": "object",
+            "properties": {
+                "title": {
+                    "type": "string",
+                    "description": "e.g. Web Developer"
+                },
+                "company": {
+                    "type": "string",
+                    "description": "e.g. Facebook"
+                },
+                "location": {
+                    "type": "string",
+                    "description": "e.g. Tel Aviv, Israel"
+                },
+                "url": {
+                    "type": "string",
+                    "description": "Position's official link",
+                    "format": "uri"
+                },
+                "job description": {
+                    "type": "string",
+                    "description": "Write a short paragraph about the position"
+                }
+            }
+        },
+        "skills": {
+            "type": "array",
+            "description": "List out the required professional skill-set for this position",
+            "additionalItems": false,
+            "items": {
+                "type": "object",
+                "additionalProperties": true,
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "e.g. Web Development"
+                    },
+                    "level": {
+                        "type": "string",
+                        "description": "e.g. Master"
+                    },
+                    "keywords": {
+                        "type": "array",
+                        "description": "List some keywords pertaining to this skill",
+                        "additionalItems": false,
+                        "items": {
+                            "type": "string",
+                            "description": "e.g. HTML"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "title": "Job Schema",
+    "type": "object"
+}
 export default function Recruiter() {
 
     return (
         <div>
-            <h1>Enter job details:</h1>
-            <form action="" autoComplete="off" className="flex flex-col space-y-4">
-                <TextField id="job-title" label="Job Title" size="small" variant="outlined" />
-                <TextField id="job-location" label="Job Location" size="small" variant="outlined" />
-                <TextField id="job-description" label="Job Description" variant="outlined" />
-                <Stack spacing={3} sx={{ width: 500 }}>
-                    <Autocomplete
-                        multiple
-                        id="tags-standard"
-                        options={skills}
-                        getOptionLabel={(option) => option}
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                                variant="standard"
-                                label="Job Skills"
-                                placeholder="Skills"
-                            />
-                        )}
-                    />
-                </Stack>
-                <Button size="small" type="submit" variant="contained" endIcon={<SendIcon />}>
-                    Submit
-                </Button>
-            </form>
+            <Form schema={schema} validator={validator} />
         </div>
     )
 }
