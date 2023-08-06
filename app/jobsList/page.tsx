da30329c8815da1555bff0044aa68258fb56aeda
shellyalmo <shellyalmo@gmail.com>
2023-08-06 20:34:10 +0300

    fix: add key to map of jobs list

--- app/jobsList/page.tsx
+++ app/jobsList/page.tsx
@@ -1,21 +1,24 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
-
+import { randomUUID } from 'crypto';
 
 const jobs = [
     {
+        'id': randomUUID(),
         'job title': 'junior frontend',
         'location': 'Tel Aviv',
         'job description': 'We are seeking a passionate and motivated Junior Frontend Developer to join our team. As a Junior Frontend Developer, you will collaborate with our experienced developers to design and implement user interfaces for web applications. Your responsibilities will include writing clean and efficient HTML, CSS, and JavaScript code, assisting in debugging and testing, and staying up-to-date with the latest frontend trends and technologies. This is an excellent opportunity for a talented individual looking to grow their skills and contribute to exciting projects in a supportive and dynamic environment.',
 
     },
     {
+        'id': randomUUID(),
         'job title': 'junior backend',
         'location': 'Tel Aviv',
         'job description': 'We are seeking a passionate and motivated Junior backend Developer to join our team. As a Junior backend Developer, you will collaborate with our experienced developers to design and implement user interfaces for web applications. Your responsibilities will include writing clean and efficient HTML, CSS, and JavaScript code, assisting in debugging and testing, and staying up-to-date with the latest backend trends and technologies. This is an excellent opportunity for a talented individual looking to grow their skills and contribute to exciting projects in a supportive and dynamic environment.',
 
     },
     {
+        'id': randomUUID(),
         'job title': 'junior fullstack',
         'location': 'Tel Aviv',
         'job description': 'We are seeking a passionate and motivated Junior fullstack Developer to join our team. As a Junior fullstack Developer, you will collaborate with our experienced developers to design and implement user interfaces for web applications. Your responsibilities will include writing clean and efficient HTML, CSS, and JavaScript code, assisting in debugging and testing, and staying up-to-date with the latest fullstack trends and technologies. This is an excellent opportunity for a talented individual looking to grow their skills and contribute to exciting projects in a supportive and dynamic environment.',
@@ -25,7 +28,7 @@ const jobs = [
 
 export default function JobsList() {
     const jobsList = jobs.map((job =>
-        <Grid item xs={8}>
+        <Grid item xs={8} key={job.id}>
             <Card sx={{ maxWidth: 345 }}>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
